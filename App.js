54459063db976cd51d8a3c098926a1deeabd9dde
furkanyilmaz <152627060+Ffurkan67@users.noreply.github.com>
2024-11-22 18:12:56 +0300

    update

--- App.js
+++ App.js
@@ -1,22 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCkJQa2M4lXIrXxFT7WKdt_kwJvBTxPABM",
-  authDomain: "tasarruf360-114b5.firebaseapp.com",
-  projectId: "tasarruf360-114b5",
-  storageBucket: "tasarruf360-114b5.firebasestorage.app",
-  messagingSenderId: "273957416221",
-  appId: "1:273957416221:web:acb321a08de039c6dec5f4",
-  measurementId: "G-B2RDS6PYWS"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 // Main App
 export default function App() {
---
+++ config.js
@@ -0,0 +1,22 @@
+//app auth realtime için import
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth";
+import "firebase/compat/database"
+
+//firebase url ayarları
+const firebaseConfig = {
+    apiKey: "AIzaSyCkJQa2M4lXIrXxFT7WKdt_kwJvBTxPABM",
+    authDomain: "tasarruf360-114b5.firebaseapp.com",
+    projectId: "tasarruf360-114b5",
+    storageBucket: "tasarruf360-114b5.firebasestorage.app",
+    messagingSenderId: "273957416221",
+    appId: "1:273957416221:web:acb321a08de039c6dec5f4",
+    measurementId: "G-B2RDS6PYWS"
+};
+
+//firebase başlatılması
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
